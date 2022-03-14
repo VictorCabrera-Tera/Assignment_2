9f03ed9bdd3559f51738dad314a595c2e405fed9
VictorCabrera-Tera <56519850+VictorCabrera-Tera@users.noreply.github.com>
2022-03-13 22:15:11 -0400

    clear all function is working for issue#5

--- script.js
+++ script.js
@@ -90,12 +90,19 @@ function fill(){
     }
 }
 
-function clearAll(){
+function clearAll()
+{
+    let allrows = document.getElementsByTagName("tr");
+
+
     for(let i = 0; i < allrows.length; i++)
     {
-        
+     	for(let j = 0; j < allrows[i].children.length; j++)
+	{
+	   allrows[i].children[j].style.backgroundColor = "white";
+	}
     }
-    alert("Clicked Clear All")
+    //alert("Clicked Clear All")
 }
 
 function fillU(){
